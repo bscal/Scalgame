642107b23f54045415f43d47360cdab498d6ec1d
bscal <jonfus123@hotmail.com>
2022-12-02 00:05:46 -0500

    Worked on creature code and moved Actor.h -> Creature.h

--- Engine/src/Core/Creature.cpp
+++ Engine/src/Core/Creature.cpp
@@ -1,43 +1,187 @@
 #include "Creature.h"
 
-#include "ResourceManager.h"
+#include "Game.h"
+#include "SMemory.h"
 
-bool CreatureInitialize(Creature* creature, const CreatureType* type)
+namespace Scal
 {
-	creature->SpriteTextureRect = type->SpriteTextureRect;
-	creature->EntityId = type->EntityId;
-	creature->Level = type->Level;
-	creature->MaxHealth = type->MaxHealth;
-	creature->MaxMana = type->MaxMana;
+namespace Creature
+{
 
-	creature->Position = { 16, 16 };
-	creature->TilePosition = { 1, 1 };
-	creature->Health = creature->MaxHealth;
-	creature->Mana = creature->MaxMana;
-	creature->Difficulty = 1;
-
-	return true;
+internal void InternalRemoveEntity(EntityMgr* mgr, uint32_t entityId)
+{
+	assert(mgr);
+	CreatureCache creatureCache = mgr->EntityMap.at(entityId);
+	mgr->EntityMap.erase(entityId);
+	if (creatureCache.IsPlayer)
+	{
+		assert(entityId < mgr->Players.size());
+		const auto& lastCreature = mgr->Players.back();
+		mgr->EntityMap[lastCreature.Id].Index = creatureCache.Index;
+		mgr->Players[creatureCache.Index] = lastCreature;
+		mgr->Players.pop_back();
+	} 
+	else
+	{
+		assert(entityId < mgr->Creatures.size());
+		const auto& lastCreature = mgr->Creatures.back();
+		mgr->EntityMap[lastCreature.Id].Index = creatureCache.Index;
+		mgr->Creatures[creatureCache.Index] = lastCreature;
+		mgr->Creatures.pop_back();
+	}
 }
 
-void CreatureUpdate(Creature* creature, Game* game)
+internal void ProcessRemoveEntities(EntityMgr* mgr)
 {
-	
+	for (size_t i = 0; i < mgr->EntitiesToRemove.size(); ++i)
+	{
+		InternalRemoveEntity(mgr, mgr->EntitiesToRemove[i]);
+	}
+	mgr->EntitiesToRemove.clear();
 }
 
-void CreatureRender(Resources* resources, Creature* creature)
+#define ESTIMATED_ENTITIES 32
+EntityMgr::EntityMgr()
+	: EntityMap(),
+	Players(),
+	Creatures(),
+	EntitiesToRemove(),
+	NextEntityId(0)
 {
-	DrawTextureRec(resources->EntitySpriteSheet,
-		creature->SpriteTextureRect,
-		creature->Position,
-		WHITE);
+	EntityMap.reserve(ESTIMATED_ENTITIES);
+	Players.reserve(1);
+	Creatures.reserve(ESTIMATED_ENTITIES);
+	EntitiesToRemove.reserve(8);
+	S_LOG_INFO("Entity Manager Initialized!");
 }
 
-void CreatureAIPathfind(Creature* creature, Game* game)
+void EntityMgr::Update(::Game* game, float dt)
+{
+	for (size_t i = 0; i < Players.size(); ++i)
+	{
+		Players[i].Update(game, dt);
+	}
+	for (size_t i = 0; i < Creatures.size(); ++i)
+	{
+		Creatures[i].Update(game, dt);
+	}
+
+	ProcessRemoveEntities(this);
+}
+
+SPlayer* EntityMgr::GetPlayer(uint32_t id)
+{
+	for (size_t i = 0; i < Players.size(); ++i)
+	{
+		if (Players[i].Id == id) return &Players[i];
+	}
+	return nullptr;
+}
+
+SCreature* EntityMgr::GetCreature(uint32_t id)
+{
+	for (size_t i = 0; i < Creatures.size(); ++i)
+	{
+		if (Creatures[i].Id == id) return &Creatures[i];
+	}
+	return nullptr;
+}
+
+SPlayer& EntityMgr::CreatePlayer()
+{
+	SPlayer p = {};
+	p.Id = NextEntityId++;
+	Players.push_back(p);
+
+	size_t index = Players.size() - 1;
+
+	CreatureCache creatureCache = {};
+	creatureCache.Index = index;
+	creatureCache.IsPlayer = true;
+	EntityMap.insert({ p.Id, creatureCache });
+
+	return Players.back();
+}
+
+SCreature& EntityMgr::CreatureCreature()
+{
+	SCreature creature = {};
+	creature.Initialize();
+	creature.Id = NextEntityId++;
+	Creatures.push_back(creature);
+
+	CreatureCache creatureCache;
+	creatureCache.Index = Creatures.size() - 1;
+	creatureCache.IsPlayer = false;
+	EntityMap.insert({ creature.Id, creatureCache });
+
+	return Creatures.back();
+}
+
+void EntityMgr::RemoveEntity(uint32_t entityId)
+{
+	if (entityId == CREATURE_EMPTY_ENTITY_ID)
+	{
+		S_LOG_ERR("Cannot remove empty entity!");
+		return;
+	}
+
+	auto creature = FindEntity(entityId);
+	if (!creature || creature->ShouldRemove)
+	{
+		S_LOG_WARN("Trying to remove an entity that is "
+			"Already marked for removal!");
+		return;
+	}
+
+	creature->ShouldRemove = true;
+	EntitiesToRemove.emplace_back(entityId);
+}
+
+SCreature* EntityMgr::FindEntity(uint32_t id)
+{
+	const auto findResult = EntityMap.find(id);
+	if (findResult == EntityMap.end())
+		return nullptr;
+
+	const auto creatureCache = findResult->second;
+	if (creatureCache.IsPlayer)
+	{
+		return &(Players[creatureCache.Index]);
+	} else
+	{
+
+		return &(Creatures[creatureCache.Index]);
+	}
+}
+
+size_t EntityMgr::TotalEntities() const
+{
+	return EntityMap.size();
+}
+
+bool EntityMgr::IsEmpty(const SCreature& creature) const
+{
+	return (creature.Id == CREATURE_EMPTY_ENTITY_ID);
+}
+
+ComponentMgr::ComponentMgr()
+	: Components()
+{
+	Components.max_load_factor(COMPONENT_LOAD_FACTOR);
+	Components.reserve(CREATURE_MAX_COMPONENTS);
+}
+
+void SCreature::Update(::Game* game, float dt)
 {
 
 }
 
-void CreatureMove(Creature* creature, int tileX, int tileY)
+void SCreature::Initialize()
 {
+	constexpr size_t size = sizeof(uint32_t) * CREATURE_MAX_COMPONENTS;
+	Scal::MemSet(&ComponentIndex, CREATURE_EMPTY_COMPONENT, size);
+}
 
+}
 }
--- Engine/src/Core/Creature.h
+++ Engine/src/Core/Creature.h
@@ -1,54 +1,186 @@
 #pragma once
 
 #include "Core.h"
+#include "Structures/SArray.h"
+#include "SUtil.h"
+
+#include <assert.h>
+#include <vector>
+#include <unordered_map>
+
+typedef uint32_t ComponentId;
+
+#define CREATURE_EMPTY_ENTITY_ID UINT32_MAX
+#define CREATURE_EMPTY_COMPONENT UINT32_MAX
+#define CREATURE_MAX_COMPONENTS 32
+#define COMPONENT_LOAD_FACTOR 0.5f
 
 struct Game;
-struct Resources;
 
-struct CreatureType
+namespace Scal
 {
-	Rectangle SpriteTextureRect;
-	const void* TypeData;
-	const char* CreatureName;
-	uint32_t EntityId;
-	uint32_t MaxHealth;
-	uint32_t MaxMana;
-	uint16_t Level;
-	uint16_t MaxLevel;
+namespace Creature
+{
+struct SPlayer;
+struct SCreature;
+
+static uint32_t SNextComponentId;
+
+template<typename T>
+struct SComponent
+{
+	static const uint32_t ID;
 };
 
-struct Creature
+template<typename T>
+const uint32_t SComponent<T>::ID = SNextComponentId++;
+
+struct SHealth : SComponent<SHealth>
 {
-	Rectangle SpriteTextureRect;
-	Vector2 Position;
-	Vector2i TilePosition;
-	uint32_t EntityId;
+	uint32_t MaxHealth;
 	uint32_t Health;
-	uint32_t MaxHealth;
-	uint32_t Mana;
-	uint32_t MaxMana;
-	uint16_t Level;
-	uint8_t Difficulty;
 };
 
-global_var uint32_t EntityIdHandler = 0;
-
-global_var const CreatureType ZOMBIE =
+struct ComponentMgr
 {
-	{ 0, 16, 16, 16 },
-	0,
-	"Zombie",
-	EntityIdHandler++,
-	10,
-	0,
-	1,
-	2
+	std::unordered_map<ComponentId, SArray,
+		std::hash<ComponentId>, std::equal_to<ComponentId>,
+		SAllocator<std::pair<const ComponentId, SArray>>> Components;
+
+	ComponentMgr();
+
+	template<typename T>
+	void RegisterComponent(ComponentId componentId)
+	{
+		assert(componentId < CREATURE_MAX_COMPONENTS);
+		if (Components.find(componentId) == Components.end())
+		{
+			// NOT FOUND
+			SArray sArray = {};
+			ArrayCreate(16, sizeof(T), &sArray);
+			Components.insert({ componentId, sArray });
+		}
+	}
+
+	template<typename T>
+	void AddComponent(SCreature* creature,
+		const SComponent<T>* component)
+	{
+		assert(component->ID < CREATURE_MAX_COMPONENTS);
+		SArray componentArray = Components[T::ID];
+		ArrayPush(&componentArray, component);
+		creature->ComponentIndex[T::ID] = componentArray.Length - 1;
+	}
+
+	template<typename T>
+	void RemoveComponent(SCreature* creature,
+		ComponentId componentId)
+	{
+		assert(componentId < CREATURE_MAX_COMPONENTS);
+		assert(creature->ComponentIndex[componentId]
+			!= CREATURE_EMPTY_COMPONENT);
+		uint32_t index = creature->ComponentIndex[T::ID];
+		creature->ComponentIndex[T::ID] = CREATURE_EMPTY_COMPONENT;
+		SArray componentArray = Components[T::ID];
+		ArrayRemoveAt(&componentArray, static_cast<uint64_t>(index));
+	}
+
+	template<typename T>
+	T* GetComponent(SCreature* creature,
+		ComponentId componentId) const
+	{
+		assert(componentId < CREATURE_MAX_COMPONENTS);
+		assert(creature->ComponentIndex[componentId]
+			!= CREATURE_EMPTY_COMPONENT);
+		uint32_t index = creature->ComponentIndex[componentId];
+		SArray componentArray = Components.at(componentId);
+		return (T*)ArrayPeekAt(&componentArray, (uint64_t)index);
+	}
 };
 
-bool CreatureInitialize(Creature* creature, const CreatureType* creatureType);
+struct CreatureCache
+{
+	uint32_t Index;
+	bool IsPlayer;
+};
 
-void CreatureUpdate(Creature* creature, Game* game);
-void CreatureRender(Resources* resources, Creature* creature);
+struct EntityMgr
+{
+	std::unordered_map<uint32_t, CreatureCache,
+		std::hash<uint32_t>, std::equal_to<uint32_t>,
+		SAllocator<std::pair<const uint32_t, CreatureCache>>> EntityMap;
+	std::vector<SPlayer, SAllocator<SPlayer>> Players;
+	std::vector<SCreature, SAllocator<SCreature>> Creatures;
+	std::vector<uint32_t, SAllocator<uint32_t>> EntitiesToRemove;
+	ComponentMgr ComponentManager;
+	uint32_t NextEntityId;
 
-void CreatureAIPathfind(Creature* creature, Game* game);
-void CreatureMove(Creature* creature, int tileX, int tileY);
+	EntityMgr();
+
+	void Update(::Game* game, float dt);
+
+	void RemoveEntity(uint32_t id);
+
+	SPlayer& CreatePlayer();
+	SCreature& CreatureCreature();
+
+	SPlayer* GetPlayer(uint32_t id);
+	SCreature* GetCreature(uint32_t id);
+	SCreature* FindEntity(uint32_t id);
+
+	size_t TotalEntities() const;
+	bool IsEmpty(const SCreature& entity) const;
+};
+
+struct SCreature
+{
+	uint32_t Id = CREATURE_EMPTY_ENTITY_ID;
+	uint32_t NetId;
+	uint32_t TypeId;
+	Rectangle TextureRect;
+	Vector2 Pos;
+	int ZIndex;
+	uint32_t ComponentIndex[CREATURE_MAX_COMPONENTS];
+	bool ShouldRemove;
+	bool IsPlayer;
+
+	void Update(::Game* game, float dt);
+
+	void Initialize();
+};
+
+struct SPlayer : SCreature
+{
+
+};
+
+inline void TestCreature(::Game* game)
+{
+	EntityMgr em = EntityMgr();
+
+	em.ComponentManager.RegisterComponent<SHealth>(SHealth::ID);
+
+	SCreature& creature = em.CreatureCreature();
+	creature.Pos.x = 5;
+	creature.Pos.y = 25;
+
+	SCreature* cPtr = em.GetCreature(creature.Id);
+
+	SHealth h;
+	h.MaxHealth = 10;
+	h.Health = 10;
+	em.ComponentManager.AddComponent<SHealth>(cPtr, &h);
+
+	SHealth* getH = em.ComponentManager.GetComponent<SHealth>(cPtr, SHealth::ID);
+
+	em.ComponentManager.RemoveComponent<SHealth>(cPtr, SHealth::ID);
+
+	em.RemoveEntity(creature.Id);
+
+	em.Update(game, 1.0f);
+
+	SCreature* cPtr2 = em.GetCreature(creature.Id);
+}
+
+}
+}
