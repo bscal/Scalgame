533fb77dd8342622691f7fbe436b4bc330c05f05
bscal <jonfus123@hotmail.com>
2023-03-03 23:59:58 -0500

    updated clang

--- Engine/src/Core/Allocators/ScalAllocator.h
+++ Engine/src/Core/Allocators/ScalAllocator.h
@@ -1,32 +1,5 @@
 #pragma once
 
-#include "Core/Core.h"
-#include "Core/Structures/STable.h"
-
-#include "rmem/rmem.h"
-
-struct SAllocator
-{
-	uint64_t Capacity;
-	void* Memory;
-};
-
-struct SAllocatorBlock
-{
-	uint32_t Capacity;
-	uint32_t Usage;
-	void* Memory;
-};
-
-struct SAllocatorPool
-{
-	STable<uint16_t, SAllocatorBlock> Blocks;
-};
-
-struct SPooledAllocator
-{
-	ObjPool CreatueAllocator;
-};
 
 
 
